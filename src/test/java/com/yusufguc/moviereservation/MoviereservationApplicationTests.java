210442ca30582c9e6a2115862df5f56b1033b6a8
yusufguc <gucyusuff@gmail.com>
2026-02-02 23:41:54 +0300

    initial commit: Spring Boot setup

---
+++ src/test/java/com/yusufguc/moviereservation/MoviereservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufguc.moviereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
